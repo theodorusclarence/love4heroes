d37e4584f768dc4ac1a8247978fbdc7dbcfd1d8e
raaynaldo <raaynaldo@hotmail.com>
2021-02-06 07:52:39 -0500

    images.js

---
+++ data/images.js
@@ -0,0 +1,14 @@
+export const images = {
+    grateful: {
+        front: '/images/card/grateful-front.png',
+        back: '/images/card/grateful-back.png',
+    },
+    love: {
+        front: '/images/card/love-front.png',
+        back: '/images/card/love-back.png',
+    },
+    meow: {
+        front: '/images/card/meow-front.png',
+        back: '/images/card/meow-back.png',
+    },
+};
