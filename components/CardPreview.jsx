4a59711a12a7133ee6c8cc8719dd47f4eeaf3e0c
Theodorus Clarence <theodorusclarence@gmail.com>
2021-02-07 17:52:53 +0700

    optimized card responsiveness

--- components/CardPreview.jsx
+++ components/CardPreview.jsx
@@ -29,15 +29,20 @@ export default function Card({ handleImage, form = defaultForm }) {
                     alt='Card'
                 />
 
-                <div className='absolute inset-0 flex flex-col px-16 py-16 space-y-10'>
-                    <div>Dear {to},</div>
-                    <div className='whitespace-pre-wrap h-3/4 font-primary text'>
+                <div className='absolute inset-0 flex flex-col px-16 py-16 space-y-10 textfield'>
+                    <div className='font-bold max-w-11/12'>Dear {to},</div>
+                    <div className='whitespace-pre-wrap h-3/4 font-primary text max-w-11/12'>
                         {msg}
                     </div>
-                    <div>
+                    <div className='font-bold max-w-11/12'>
                         <div>Love,</div>
                         <div>{from}</div>
                     </div>
+                    <style jsx>{`
+                        .textfield {
+                            font-size: min(2.4vw, 1rem);
+                        }
+                    `}</style>
                 </div>
             </div>
         </ReactCardFlip>
